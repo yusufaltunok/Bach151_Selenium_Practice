0e5f498300aa37c88d542366141b330ec7bd9bd7
yusufaltunok <yusuf.66.77@gmail.com>
2023-06-05 14:34:23 +0300

    Initial commit

---
+++ src/test/java/day01_practice/C01.java
@@ -0,0 +1,15 @@
+package day01_practice;
+
+import org.openqa.selenium.WebDriver;
+
+public class C01 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/day01_practice/C02.java
@@ -0,0 +1,2 @@
+package day01_practice;public class C02 {
+}
